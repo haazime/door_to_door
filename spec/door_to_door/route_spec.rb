ef7fece52744b928be4af676a2f1535f6ebbb3d7
haazime <h4zime@gmail.com>
2017-10-11 17:51:58 +0900

    refactor

--- spec/door_to_door/route_spec.rb
+++ spec/door_to_door/route_spec.rb
@@ -7,55 +7,53 @@ module Admin
   BarController = Class.new
 end
 
-module DoorToDoor
-  RSpec.describe Route do
-    let(:route) do
-      described_class.new(app_route)
-    end
+RSpec.describe DoorToDoor::Route do
+  let(:route) do
+    described_class.new(app_route)
+  end
 
-    describe '#controller_class' do
-      context 'FooController' do
-        let(:app_route) do
-          OpenStruct.new(controller: 'foo', action: 'action', verb: 'VERB')
-        end
-
-        it do
-          expect(route.controller_class).to eq(FooController)
-        end
-      end
-
-      context 'Admin::BarController' do
-        let(:app_route) do
-          OpenStruct.new(controller: 'admin/bar', action: 'action', verb: 'VERB')
-        end
-
-        it do
-          expect(route.controller_class).to eq(Admin::BarController)
-        end
-      end
-    end
-
-    describe '#action' do
+  describe '#controller_class' do
+    context 'FooController' do
       let(:app_route) do
-        OpenStruct.new(controller: 'foo', action: 'action', verb: 'VERB')
+        OpenStruct.new(controller: 'foo')
       end
 
       it do
-        expect(route.action).to eq('action')
+        expect(route.controller_class).to eq(FooController)
       end
     end
 
-    describe '#request' do
+    context 'Admin::BarController' do
       let(:app_route) do
-        OpenStruct.new(controller: 'foo', action: 'action', verb: 'VERB')
+        OpenStruct.new(controller: 'admin/bar')
       end
 
       it do
-        spy = RequestReceiverSpy.new
-        route.request(spy)
-        expect(spy.method).to eq('verb')
-        expect(spy.action).to eq('action')
+        expect(route.controller_class).to eq(Admin::BarController)
       end
+    end
+  end
+
+  describe '#action' do
+    let(:app_route) do
+      OpenStruct.new(action: 'action')
+    end
+
+    it do
+      expect(route.action).to eq('action')
+    end
+  end
+
+  describe '#request' do
+    let(:app_route) do
+      OpenStruct.new(action: 'action', verb: 'VERB')
+    end
+
+    it do
+      spy = RequestReceiverSpy.new
+      route.request(spy)
+      expect(spy.method).to eq('verb')
+      expect(spy.action).to eq('action')
     end
   end
 end
